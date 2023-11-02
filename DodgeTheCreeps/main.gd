441095c777ca46676d6e77c3ef8b64236d946b7a
Joel Encinas <joelencinasmartin@gmail.com>
2023-11-02 16:48:48 +0100

    instantiate mob and add it to the scene

--- DodgeTheCreeps/main.gd
+++ DodgeTheCreeps/main.gd
@@ -21,7 +21,22 @@ func new_game():
 	$StartTimer.start()
 
 func _on_mob_timer_timeout():
-	pass # Replace with function body.
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	mob.position = mob_spawn_location.position
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
 
 func _on_score_timer_timeout():
 	score += 1

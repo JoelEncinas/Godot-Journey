17ff80828d59323aaee1084036c7a4f4b9598ff6
Joel Encinas <joelencinasmartin@gmail.com>
2023-11-03 14:28:26 +0100

    addes music theme and death sound

--- DodgeTheCreeps/main.gd
+++ DodgeTheCreeps/main.gd
@@ -10,11 +10,14 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
